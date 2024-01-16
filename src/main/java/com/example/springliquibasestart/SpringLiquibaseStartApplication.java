166a50f5f2a127b73f93fcd6ad520211d46ac330
TDF-SplitCore <timeryasovdf@gmail.com>
2024-01-16 18:10:21 +0500

    Liquibase create

---
+++ src/main/java/com/example/springliquibasestart/SpringLiquibaseStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.springliquibasestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiquibaseStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiquibaseStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springliquibasestart/SpringLiquibaseStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springliquibasestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
